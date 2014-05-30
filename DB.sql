054f3ad07408b61d72bd595aa83e11d018d47e33
MikeO7 <Doyle@doyles-mbp.app.byu.edu>
2014-05-29 22:27:36 -0600

    Table Changes

--- DB.sql
+++ DB.sql
@@ -1,9 +1,6 @@
-DROP DATABASE IF EXISTS IT;
-
-CREATE DATABASE IT;
+CREATE DATABASE IF NOT EXISTS IT;
 
 USE IT;
-
 
 DROP TABLE IF EXISTS Customers;
 DROP TABLE IF EXISTS Employees;
@@ -18,14 +15,13 @@ DROP TABLE IF EXISTS Supplies;
 DROP TABLE IF EXISTS ProductSupplier;
 
 
-
 CREATE TABLE Customers (
          CustomerId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
          CompanyName VARCHAR(30),
          ContactName VARCHAR(30),
          MobilePhone VARCHAR(10),
          Email VARCHAR(30),
-         Address VARCHAR(30),
+         Address VARCHAR(100),
          City VARCHAR(30),
          State VARCHAR(30),
          Country VARCHAR(30),
@@ -52,10 +48,67 @@ CREATE TABLE Employees (
 CREATE TABLE Products (
          ProductCode INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
          Name VARCHAR(30),
-         Description VARCHAR(400),
+         Description VARCHAR(1000),
          SuggestedUnitPrice DOUBLE(2.2), 
          BuyUnitPrice DOUBLE(2.2), 
          UnitsInStock INT
+       );
+
+CREATE TABLE Orders (
+         OrderId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         CustomerId VARCHAR(30), --FK
+         EmployeeId VARCHAR(30), --FK
+         OrderedDate DATE, 
+         RequiredDate DATE, 
+         OrderStatus INT, --??????
+         ShippedDate Date,
+         ShipperID VARCHAR(30),
+         ShipToName VARCHAR(30),
+         ShipToAddress VARCHAR(30),
+         ShiToCity VARCHAR(30),
+         ShiptTCountry VARCHAR(30),
+         ShipToPostalCode VARCHAR(5)
+       );
+
+CREATE TABLE Invoices (
+         InvoiceId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         OrderId VARCHAR(30),
+         InvoiceDate DATE, 
+       );
+
+CREATE TABLE ProductDetails (
+         ProductCode INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         MoreDescription VARCHAR(1000) 
+       );
+
+CREATE TABLE Shippers (
+         ShipperId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         CompanyName VARCHAR(30),
+         ContactName VARCHAR(30),
+         ContactPhone VARCHAR(10) 
+       );
+
+CREATE TABLE Payments (
+         PrderId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         PaymentDate Date,
+         Amount Double(2,2)
+       );
+
+CREATE TABLE Supplies (
+         SupplierId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         CompanyName VARCHAR(30),
+         ContactName VARCHAR(30),
+         PhoneMobile VARCHAR(30),
+         Email VARCHAR(30),
+         Address VARCHAR(30),
+         City VARCHAR(30),
+         PostalCode VARCHAR(5), 
+       );
+
+CREATE TABLE ProductSupplier (
+         ProductCode INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         SupplierId VARCHAR(30),
+         Notes VARCHAR(1000),
        );
 
 
