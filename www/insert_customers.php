1ecf1c062126656b5aff2b2926a8bde0ca7939f8
Mike <Mike>
2014-06-11 17:12:35 -0600

    Prints Tables out

--- www/insert_customers.php
+++ www/insert_customers.php
@@ -26,3 +26,70 @@ echo "1 record added";
 
 mysqli_close($con);
 ?>
+
+<html>
+<header><title>Read Without Indexing</title>
+
+<link href="css/bootstrap.css" rel="stylesheet">
+</header>
+<body>
+
+</body>
+</html>
+
+
+<?php
+
+// EXAMPLE
+PrintTable("SELECT * FROM Customers");
+function PrintTable($query)
+{ 
+    // Connect
+    $mysqli = new mysqli("localhost", "root", "root");
+    if(!$mysqli) {
+        die();
+    }
+    mysqli_select_db($mysqli, "Test");
+    if(!$mysqli) {
+        die();
+    }
+    if (!($res = $mysqli->query($query))) {
+        if($mysqli) $mysqli->close();
+        return;
+    }
+ 
+    echo "<table class=\"table table-striped\">"; 
+ 
+    // Print the table's header row
+    $numFields = $mysqli->field_count;
+    echo "<tr>";
+    $finfo = $res->fetch_fields();
+    foreach ($finfo as $val) {
+        echo "<td><b>" . $val->name . "</b></td>";
+    }
+    echo "</tr>";
+ 
+    // Print each row
+    $i = 0;
+    while($row = $res->fetch_row())
+    {
+        
+ 
+        for($j = 0; $j < $numFields; $j++)
+        {
+            echo "<td>" . $row[$j] . "</td>";
+        }
+ 
+        echo "</tr>";
+        $i++;
+    }
+ 
+    echo "</table><br/><br/>";  
+ 
+    // Free resources
+    if($res) $res->free();    
+    if($mysqli) $mysqli->close();   
+}
+
+
+?>
--- www/update_employees.php
+++ www/update_employees.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 $con=mysqli_connect("localhost","root","root","Test");
 // Check connection
@@ -17,4 +20,71 @@ mysqli_query($con, "UPDATE Employees SET Address = '$new_address', City = '$new_
 echo "1 record updated";
 
 mysqli_close($con);
+?>
+
+<html>
+<header><title>Read Without Indexing</title>
+
+<link href="css/bootstrap.css" rel="stylesheet">
+</header>
+<body>
+
+</body>
+</html>
+
+
+<?php
+
+// EXAMPLE
+PrintTable("SELECT * FROM Employees");
+function PrintTable($query)
+{ 
+    // Connect
+    $mysqli = new mysqli("localhost", "root", "root");
+    if(!$mysqli) {
+        die();
+    }
+    mysqli_select_db($mysqli, "Test");
+    if(!$mysqli) {
+        die();
+    }
+    if (!($res = $mysqli->query($query))) {
+        if($mysqli) $mysqli->close();
+        return;
+    }
+ 
+    echo "<table class=\"table table-striped\">"; 
+ 
+    // Print the table's header row
+    $numFields = $mysqli->field_count;
+    echo "<tr>";
+    $finfo = $res->fetch_fields();
+    foreach ($finfo as $val) {
+        echo "<td><b>" . $val->name . "</b></td>";
+    }
+    echo "</tr>";
+ 
+    // Print each row
+    $i = 0;
+    while($row = $res->fetch_row())
+    {
+        
+ 
+        for($j = 0; $j < $numFields; $j++)
+        {
+            echo "<td>" . $row[$j] . "</td>";
+        }
+ 
+        echo "</tr>";
+        $i++;
+    }
+ 
+    echo "</table><br/><br/>";  
+ 
+    // Free resources
+    if($res) $res->free();    
+    if($mysqli) $mysqli->close();   
+}
+
+
 ?>
